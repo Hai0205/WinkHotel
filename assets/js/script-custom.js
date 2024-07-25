55613d8cf6c192ccff57ed3896f64bfcbb01d8fb
MNguyen1302 <mynguyenn130202@gmail.com>
2024-07-25 14:08:43 +0700

    update dropdown

--- assets/js/script-custom.js
+++ assets/js/script-custom.js
@@ -37,25 +37,46 @@ function enableSwiper() {
   // }
 }
 
-function toggleDropdown(e) {
-  document.onclick = function (e) {
-    const dropdownMenu = document.querySelector(".dropdown-custom__menu");
-    const dropdownItems = document.querySelectorAll(".dropdown-custom__item");
-    const btnDropdown = document.querySelector(".dropdown-custom__btn h5");
-    if (e.target.parentElement.classList.contains("dropdown-custom__btn") ||
-      e.target.parentElement.classList.contains("dropdown-custom")
-    ) {
+function toggleDropdown() {
+  const dropdowns = document.querySelectorAll(".dropdown-custom")
+
+  dropdowns.forEach(dropdown => {
+    const btnDropdown = dropdown.querySelector('.dropdown-custom__btn')
+    const dropdownMenu = dropdown.querySelector(".dropdown-custom__menu");
+    const dropdownItems = dropdown.querySelectorAll(".dropdown-custom__item");
+    const textDropdown = dropdown.querySelector(".dropdown-custom__text");
+    
+    btnDropdown.onclick = function (e) {
+      closeAllDropdowns(dropdown)
       dropdownMenu.classList.toggle("dropdown--active")
-    } else {
-      dropdownMenu.classList.remove("dropdown--active")
+    }
+
+    document.onclick = function (e) {
+      if (!e.target.closest(".dropdown-custom")) {
+        closeAllDropdowns();
+      }
     }
 
     dropdownItems.forEach(item => {
-      item.onclick = function (e) {
-        btnDropdown.textContent = e.target.textContent;
+      item.onclick = function(e) {
+        let tmp = textDropdown.textContent;
+        textDropdown.textContent = e.target.textContent;
+        if (e.target.classList.contains("language__item")) {
+          item.textContent = tmp
+        }
+        closeAllDropdowns()
       }
     })
-  }
+
+    function closeAllDropdowns(exception) {
+      dropdowns.forEach(dropdown => {
+        if (dropdown !== exception) {
+          const menu = dropdown.querySelector('.dropdown-custom__menu');
+          menu.classList.remove('dropdown--active');
+        }
+      });
+    }
+  })
 }
 
 toggleDropdown()

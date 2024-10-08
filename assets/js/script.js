b7ee84d597b3853cadae3982ebf0226c068f115a
hai <hai.alphacreative@gmail.com>
2024-10-08 09:29:49 +0700

    add language

--- assets/js/script.js
+++ assets/js/script.js
@@ -599,11 +599,23 @@ function toggleDropdown() {
     $dropdownItems.on("click", function (e) {
       e.stopPropagation();
       const $item = $(this);
-      const tmp = $textDropdown.text();
+      const tmpText = $textDropdown.text();
+      const tmpImgSrc = $textDropdown.find("img").attr("src"); // Get the current image src if present
+      const $img = $item.find("img"); // Check if the clicked item contains an img
+
+      // Swap text content
       $textDropdown.text($item.text());
-      if ($item.hasClass("language__item")) {
-        $item.text(tmp);
+
+      // If the item has an image, swap the img src
+      if ($img.length) {
+        $textDropdown.html($item.html()); // Swap the entire HTML, including the img
+        $item.html(
+          `${tmpImgSrc ? `<img src="${tmpImgSrc}" />` : ""} ${tmpText}`
+        ); // Swap img and text back to the item
+      } else if ($item.hasClass("language__item")) {
+        $item.text(tmpText);
       }
+
       closeAllDropdowns();
     });
 
